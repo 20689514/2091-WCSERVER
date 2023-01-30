4c2e0d8b6d75f44e6b7e11267a4b7493f1055972
20689514 <119300106+20689514@users.noreply.github.com>
2023-01-30 12:51:15 +0800

    1 file modified

--- prelims/PRELIM_ACTIVITIES/Prelim-lab-activity3/NodeJS-Application/Lab-Act3-NodeJS-Server.js
+++ prelims/PRELIM_ACTIVITIES/Prelim-lab-activity3/NodeJS-Application/Lab-Act3-NodeJS-Server.js
@@ -3,20 +3,41 @@
 //Section: WD-201
 
 var http = require('http');
-
 var server = http.createServer(function (req, res) {
   if (req.url == '/') {
+    //set response header
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
-    res.write('<html><body><p>This is home Page.</p></body></html>');
+    //set response content
+    res.write(
+      '<html><body><h1>Welcome to my Node.js Application.</h1><p>Welcome John Smith. This is an activity about basics of Node.js</p></body></html>'
+    );
     res.end();
-  } else if (req.url == '/student') {
+  } else if (req.url == '/about') {
+    //set response header
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<html><body><p>This is student Page.</p></body></html>');
+
+    //set response content
+    res.write(
+      '<html><body><h1>This is the About Page.</h1><p>Hello John Smith. This activity will teach on how to deal with a simple server and local modules in Node.js</p></body></html>'
+    );
+    res.end();
+  } else if (req.url == '/contact') {
+    //set response header
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    //set response content
+    res.write(
+      '<html><body><h1>This is the Contact Page.</h1><p>John Smith, if you want additional details about this activity go to this site: https://tutorialteacher.com/nodejs-tutorials</p></body></html>'
+    );
+    res.end();
+  } else if (req.url == '/gallery') {
+    //set response header
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    //set response content
+    res.write('<html><body><h1>This is the Gallery Page.</h1></body></html>');
     res.end();
   } else res.end('Invalid Request!');
 });
-
 server.listen(5000);
-
 console.log('Node.js web server at port 5000 is running..');
