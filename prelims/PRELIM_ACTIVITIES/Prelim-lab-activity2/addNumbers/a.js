428ea9d96cec30bc9e446480a91eb7c400acc421
20689514 <119300106+20689514@users.noreply.github.com>
2023-01-30 21:13:34 +0800

    2 files modified

--- prelims/PRELIM_ACTIVITIES/Prelim-lab-activity2/addNumbers/a.js
+++ prelims/PRELIM_ACTIVITIES/Prelim-lab-activity2/addNumbers/a.js
@@ -1,2 +1,9 @@
 exports.rate = 300;
-exports.tax = 300;
+exports.tax = 720;
+exports.sss = 1200;
+exports.pagibig = 300;
+exports.phealth = 400;
+exports.deductions =
+  exports.tax + exports.sss + exports.pagibig + exports.phealth;
+exports.grossS = exports.rate * 24;
+exports.netS = exports.grossS - exports.deductions;
--- prelims/PRELIM_ACTIVITIES/Prelim-lab-activity2/addNumbers/b.js
+++ prelims/PRELIM_ACTIVITIES/Prelim-lab-activity2/addNumbers/b.js
@@ -6,7 +6,7 @@ http
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('<br>Quotient: ' + calc.rate);
 
-    res.write('<br>Quotient: ' + calc.rate);
+    res.write('<br>Quotient: ' + calc.tax);
 
     res.end();
   })
