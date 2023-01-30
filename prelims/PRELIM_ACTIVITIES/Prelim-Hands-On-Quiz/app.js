98dc2cc05d231dfed4e0414048f6bcb5ea765c40
20689514 <119300106+20689514@users.noreply.github.com>
2023-01-30 20:17:32 +0800

    2 files modified

--- prelims/PRELIM_ACTIVITIES/Prelim-Hands-On-Quiz/app.js
+++ prelims/PRELIM_ACTIVITIES/Prelim-Hands-On-Quiz/app.js
@@ -8,7 +8,7 @@ http
     res.write('<br>Difference: ' + calc.subtract(6, 2));
     res.write('<br>Product: ' + calc.multiply(6, 2));
     res.write('<br>Quotient: ' + calc.divide(6, 2));
-    res.write('<br>Quotient: ' + calc.rateperhr());
+    res.write('<br>Quotient: ' + calc.rateperhr);
 
     res.end();
   })
