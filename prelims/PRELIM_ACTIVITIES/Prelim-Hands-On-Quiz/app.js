7d13808b862e662c7e7988a14fdf116e2ec89776
20689514 <119300106+20689514@users.noreply.github.com>
2023-01-30 19:49:29 +0800

    1 file modified

--- prelims/PRELIM_ACTIVITIES/Prelim-Hands-On-Quiz/app.js
+++ prelims/PRELIM_ACTIVITIES/Prelim-Hands-On-Quiz/app.js
@@ -4,10 +4,10 @@ var calc = require('./calculation.js');
 http
   .createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Sum: ' + calc.add(1, 2));
-    res.write('Difference: ' + calc.subtract(1, 2));
-    res.write('Product: ' + calc.multiply(1, 2));
-    res.write('Quotient: ' + calc.divide(1, 2));
+    res.write('Sum: ' + calc.add(6, 2));
+    res.write('<br>Difference: ' + calc.subtract(6, 2));
+    res.write('<br>Product: ' + calc.multiply(6, 2));
+    res.write('<br>Quotient: ' + calc.divide(6, 2));
     res.end();
   })
   .listen(8080);
