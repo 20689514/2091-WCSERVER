0b5ecb5e9840c9fc1ae1b01da41cc20e1f001c47
20689514 <119300106+20689514@users.noreply.github.com>
2023-01-30 21:06:19 +0800

    2 files modified

--- prelims/PRELIM_ACTIVITIES/Prelim-lab-activity2/addNumbers/a.js
+++ prelims/PRELIM_ACTIVITIES/Prelim-lab-activity2/addNumbers/a.js
@@ -1,1 +1,26 @@
 exports.rate = 300;
+exports.tax = 720;
+exports.sss = 1200;
+exports.pagibig = 300;
+exports.phealth = 400;
+exports.deductions =
+  exports.tax + exports.sss + exports.pagibig + exports.phealth;
+exports.grossS = exports.rate * 24;
+exports.netS = exports.grossS - exports.deductions;
+
+var calcu = {
+  add: function add(a, b) {
+    return a + b;
+  },
+  subtract: function subtract(a, b) {
+    return a - b;
+  },
+  multiply: function multiply(a, b) {
+    return a * b;
+  },
+  divide: function divide(a, b) {
+    return a / b;
+  },
+};
+
+module.exports = calcu;
--- prelims/PRELIM_ACTIVITIES/Prelim-lab-activity2/addNumbers/b.js
+++ prelims/PRELIM_ACTIVITIES/Prelim-lab-activity2/addNumbers/b.js
@@ -4,8 +4,11 @@ var http = require('http');
 http
   .createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-
-    res.write('<br>Quotient: ' + calc.rate);
+    res.write('Sum: ' + calc.add(6, 2));
+    res.write('<br>Difference: ' + calc.subtract(6, 2));
+    res.write('<br>Product: ' + calc.multiply(6, 2));
+    res.write('<br>Quotient: ' + calc.divide(6, 2));
+    res.write('<br>Quotient: ' + calc.tax);
 
     res.end();
   })
