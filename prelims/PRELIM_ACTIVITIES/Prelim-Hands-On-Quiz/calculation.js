1ffd20772b4f9e358298eb1f10e7a32bce627e9c
20689514 <119300106+20689514@users.noreply.github.com>
2023-01-30 20:56:57 +0800

    2 files modified

--- prelims/PRELIM_ACTIVITIES/Prelim-Hands-On-Quiz/calculation.js
+++ prelims/PRELIM_ACTIVITIES/Prelim-Hands-On-Quiz/calculation.js
@@ -1,3 +1,13 @@
+exports.rate = 300;
+exports.tax = 720;
+exports.sss = 1200;
+exports.pagibig = 300;
+exports.phealth = 400;
+exports.deductions =
+  exports.tax + exports.sss + exports.pagibig + exports.phealth;
+exports.grossS = exports.rate * 24;
+exports.netS = exports.grossS - exports.deductions;
+
 var calcu = {
   add: function add(a, b) {
     return a + b;
@@ -14,13 +24,3 @@ var calcu = {
 };
 
 module.exports = calcu;
-
-exports.rateperhr = 300;
-exports.tax = 720;
-exports.sss = 1200;
-exports.pagibig = 300;
-exports.phealth = 400;
-exports.deductions =
-  exports.tax + exports.sss + exports.pagibig + exports.phealth;
-exports.grossS = exports.rateperhr * 24;
-exports.netS = exports.grossS - exports.deductions;
