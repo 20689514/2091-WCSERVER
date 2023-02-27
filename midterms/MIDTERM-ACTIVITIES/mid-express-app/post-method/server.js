286077cca6fe2df51c2b64e14846fc511cadd84d
20689514 <119300106+20689514@users.noreply.github.com>
2023-02-27 11:58:04 +0800

    1 file modified

--- midterms/MIDTERM-ACTIVITIES/mid-express-app/post-method/server.js
+++ midterms/MIDTERM-ACTIVITIES/mid-express-app/post-method/server.js
@@ -1,25 +1,22 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
 var bodyParser = require('body-parser');
 
-var urlencodedParser = bodyParser.urlencoded({extended:false})
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-
-app.get('/', function (req, res){
-    res.sendFile( __dirname + '/' + 'index.html');
-})
-
-app.post('/process_post', urlencodedParser, function (req, res) {
-    response = {
-        first_name:req.query.fname,
-        last_name:req.query.lname
-    };
-
-console.log(response);
-res.end(JSON.stringify(response));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/' + 'index.html');
 });
 
+app.post('/process_post', urlencodedParser, function (req, res) {
+  response = {
+    first_name: req.body.fname,
+    last_name: req.body.lname,
+  };
+  console.log(response);
+  res.end(JSON.stringify(response));
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
