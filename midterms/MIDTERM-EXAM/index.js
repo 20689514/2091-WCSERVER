da9ee2f227e4cda56839245c607c7e4c66fbfda3
Chrisjohn Crisostomo <119300106+20689514@users.noreply.github.com>
2023-03-22 16:42:33 +0800

    Add files via upload

---
+++ midterms/MIDTERM-EXAM/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+const dishes =[{
+    type: 'Sisig',
+    province: 'Pampanga',
+    price: 220
+},
+{
+    type: 'Salpicao',
+    province: 'Quezon',
+    price: 180
+},
+{
+    type: 'Bagnet',
+    province: 'Ilocos',
+    price: 370
+}
+]; 
+app.get('/dishes', (req, res) => {
+    res.send(dishes);
+});
+
+app.get('/dishes/:type', (req, res) => {
+    const dishes = dishes.find(a => a.type=== parseInt(req.params.type));
+    if (!dishes) return res.status(404).send('Record not found');
+    res.send(dishes);
+});
+
+app.listen(3000, () => console.log('server is listening on port 3000'));
